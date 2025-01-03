848bcce40eb67d9df12e2a9bf2c23680d0f12035
kousarshah <162299975+kousarshah@users.noreply.github.com>
2025-01-03 18:55:06 +0500

    Update BlogCard.tsx

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -18,7 +18,7 @@ export default function BlogCard({ post, isDarkBackground }: BlogCardProps) {
         isDarkBackground ? "bg-slate-800 text-white" : "text-slate-800"
       } rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300`}
     >
-      <img
+      <Image
         src={post.imageUrl}
         alt={post.title}
         className="w-full h-48 object-cover rounded-t-lg"
