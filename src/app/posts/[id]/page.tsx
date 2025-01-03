26de3586fa281038079b930c1180c9fe3af1492b
kousarshah <162299975+kousarshah@users.noreply.github.com>
2025-01-03 18:47:57 +0500

    Update page.tsx

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -154,6 +154,7 @@ export default function Post({ params }: { params: { id: string } }) {
       </p>
     ));
   };
+  renderParagraphs();
 
   return (
     <div className="max-w-3xl mx-auto p-5">
